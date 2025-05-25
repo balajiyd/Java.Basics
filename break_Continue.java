abb4e92d788456cb07627e3cc5fee2aced473e38
balajiyd <124142672+balajiyd@users.noreply.github.com>
2025-05-25 10:07:12 +0530

    Create break_Continue.java

---
+++ break_Continue.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class New{
+  public static void main(String[] args) throws InterruptedException{
+   Scanner sc = new Scanner(System.in);
+    for(int i=1;i<=10;i++){
+      /*if(i==5){
+        break;
+      }*/
+     if(i==5){
+      continue;
+     }
+      System.out.print(i+" ");
+    }
+    
+
+  
+  }
+}
